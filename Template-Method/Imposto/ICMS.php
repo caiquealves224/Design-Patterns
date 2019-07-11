7076dc32527bc8bcfd1c00134d481edfa5e1726f
Caique Alves dos Santos <caiquealves224@gmail.com>
2019-07-11 14:16:31 -0300

    if nos impostos

--- Template-Method/Imposto/ICMS.php
+++ Template-Method/Imposto/ICMS.php
@@ -4,6 +4,9 @@ class ICMS implements Imposto
 {
     public function calcula(Orcamento $Orcamento)
     {
+        if($Orcamento->getValor() > 500 ) {
+            return $Orcamento->getValor() * 0.05;    
+        }
         return $Orcamento->getValor() * 0.05;
     }
 }
--- Template-Method/Imposto/ISS.php
+++ Template-Method/Imposto/ISS.php
@@ -4,6 +4,9 @@ class ISS implements Imposto
 {
     public function calcula(Orcamento $Orcamento)
     {
+        if($Orcamento->getValor() > 300){
+            return $Orcamento->getValor() * 0.15;
+        }
         return $Orcamento->getValor() * 0.10;
     }
 }
