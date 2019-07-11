2fc250e8efd38288da38a80c1dceb1589d1b7ac4
Caique Alves dos Santos <caiquealves224@gmail.com>
2019-07-11 14:50:38 -0300

    acertar ordem dos imports

--- Template-Method/index.php
+++ Template-Method/index.php
@@ -1,8 +1,8 @@
 <?php
     require "Orcamento.php";
     require "CalculadoraDeImpostos.php";
-    require "TemplateDeImpostoCondicional.php";
     require "Imposto/Imposto.php";
+    require "TemplateDeImpostoCondicional.php";
     require "Imposto/ICMS.php";
     require "Imposto/ISS.php";
     require "Imposto/KCV.php";
