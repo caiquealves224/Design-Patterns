6a8eda07fc6c6b88515013a51b04f40487bc278a
Caique Alves <caiquealves224@gmail.com>
2019-07-14 00:05:58 -0300

    adicionanado transação de estado na classe finalizado

--- State/Estado/Finalizado.php
+++ State/Estado/Finalizado.php
@@ -6,4 +6,17 @@ class Finalizado implements IEstado
     {
         throw new Exception("Orçamento Finalizado não pode receber descontos", 1);
     }
+
+    public function Aprova(Orcamento $Orcamento){
+        throw new Exception("Orçamento já finalziado", 1);
+    }
+
+    public function Reprova(Orcamento $Orcamento){
+        throw new Exception("Orcamento já finalizado", 1);
+    }
+
+    public function Finaliza(Orcamento $Orcamento){
+        throw new Exception("Orçamento já finalziado", 1);
+    }
+
 }
